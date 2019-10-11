3785c4feb9b8552b28f1554146044a2efc3e4354
SDISON <sdisonsinstein123@gmail.com>
2019-10-11 13:03:03 +0530

    INTIALIZE

--- main/ddemail.py
+++ main/ddemail.py
@@ -3,8 +3,8 @@ def em(address,task):
 	fromaddr = 'singhdhruv498@gmail.com'
 	toaddrs  = str(address)
 	msg = "RICHIE IS GOING FOR "+str(task)
-	username = 'singhdhruv498@gmail.com'
-	password = 'iamdhrmsha'
+	username = ''
+	password = ''
 	server = smtplib.SMTP('smtp.gmail.com:587')
 	server.starttls()
 	server.login(username,password)
